629818896dccad4a3d0db4482a739efa58548fa9
pmacau <pmacau@students.cs.ubc.ca>
2025-03-08 15:54:56 -0800

    commented flag

--- src/flag_system.hpp
+++ src/flag_system.hpp
@@ -33,52 +33,52 @@ public:
     void step() {
         //if () screenstate no longer
         // set unpaused to false. 
-        auto screen_entity = registry.view<ScreenState>().front(); 
-        auto& screen_state = registry.get<ScreenState>(screen_entity);
-        // if (screen_state.)
-        if (done) {
-            return; 
-        }
-        if (tutorial_step == TutorialStep::None) {
-            auto view = registry.view<Motion, Player>();
-            for (auto entity : view) {
-                auto& motion = view.get<Motion>(entity);
-                if (motion.velocity.x != 0.0f || motion.velocity.y != 0.0f) {
-                    setMoved(true);
-                    break;
-                }
-            }
-        }
-        else if (tutorial_step == TutorialStep::Moved) {
-            auto view = registry.view<ScreenState>();
-            setAccessed(true); // TODO: REMOVE THIS WHEN YOU CHANGE BELOW THIS IS JUST FOR PASS THROUGH
-            is_paused = true; //TODO: Move below 
-            for (auto entity : view) {
-                //TODO:: Frank just change the if statement to whatever state gets changed in registry and it should work. 
-                //auto& screen = registry.get<ScreenState>(entity);
-                //if (screen.darken_screen_factor > 0.0f) {  
-                //    setAccessed(true);
-                //    break;
-                //}
-            }
-        }
-        else if (tutorial_step == TutorialStep::Accessed) {
-            auto proj_view = registry.view<Projectile>();
-            if (!proj_view.empty()) {
-                setShot(true); // just leave it at Shot and then display the text. 
-            }
-        }
-        /*else if (tutorial_step == TutorialStep::Shot) { Don't really know a good way about going about this, we can leave this out since it should be enough. 
-        *  You can leave a message after shooting to tell the player to go explore... 
-            auto mob_view = registry.view<Mob>();  
-            for (auto entity : mob_view) {
-                auto& mob = registry.get<Mob>(entity);
-                if (mob.health <= 0) {
-                    setMobKilled(true);
-                    break;
-                }
-            }
-        }*/
+        //auto screen_entity = registry.view<ScreenState>().front(); 
+        //auto& screen_state = registry.get<ScreenState>(screen_entity);
+        //// if (screen_state.)
+        //if (done) {
+        //    return; 
+        //}
+        //if (tutorial_step == TutorialStep::None) {
+        //    auto view = registry.view<Motion, Player>();
+        //    for (auto entity : view) {
+        //        auto& motion = view.get<Motion>(entity);
+        //        if (motion.velocity.x != 0.0f || motion.velocity.y != 0.0f) {
+        //            setMoved(true);
+        //            break;
+        //        }
+        //    }
+        //}
+        //else if (tutorial_step == TutorialStep::Moved) {
+        //    auto view = registry.view<ScreenState>();
+        //    setAccessed(true); // TODO: REMOVE THIS WHEN YOU CHANGE BELOW THIS IS JUST FOR PASS THROUGH
+        //    is_paused = true; //TODO: Move below 
+        //    for (auto entity : view) {
+        //        //TODO:: Frank just change the if statement to whatever state gets changed in registry and it should work. 
+        //        //auto& screen = registry.get<ScreenState>(entity);
+        //        //if (screen.darken_screen_factor > 0.0f) {  
+        //        //    setAccessed(true);
+        //        //    break;
+        //        //}
+        //    }
+        //}
+        //else if (tutorial_step == TutorialStep::Accessed) {
+        //    auto proj_view = registry.view<Projectile>();
+        //    if (!proj_view.empty()) {
+        //        setShot(true); // just leave it at Shot and then display the text. 
+        //    }
+        //}
+        ///*else if (tutorial_step == TutorialStep::Shot) { Don't really know a good way about going about this, we can leave this out since it should be enough. 
+        //*  You can leave a message after shooting to tell the player to go explore... 
+        //    auto mob_view = registry.view<Mob>();  
+        //    for (auto entity : mob_view) {
+        //        auto& mob = registry.get<Mob>(entity);
+        //        if (mob.health <= 0) {
+        //            setMobKilled(true);
+        //            break;
+        //        }
+        //    }
+        //}*/
     }
 
   
