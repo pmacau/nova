b025f7abf53be876a96f50def877971995ef6b75
pmacau <pmacau@students.cs.ubc.ca>
2025-04-06 11:28:42 -0700

    just have to do despawn logic, and maybe fix the spawn since I only got 3/4?

--- src/creature/boss_def.hpp
+++ src/creature/boss_def.hpp
@@ -17,8 +17,21 @@ struct BossSpawn {
 inline void initBossSpawnData(std::vector<BossSpawn>& bossSpawns) {
     bossSpawns.clear();
     auto& bossIndices = MapSystem::getBossSpawnIndices();
-
+	std::cout << "Boss spawn indices size: " << bossIndices.size() << std::endl;
+     
     for (auto& bossIndex : bossIndices) {
-        bossSpawns.push_back({bossIndex, false, CreatureID::BOSS}); // TODO: link to creature def
+
+		if (MapSystem::get_biome_by_indices(bossIndex) == Biome::B_ICE) {
+			bossSpawns.push_back({ bossIndex, false, CreatureID::BOSS_BEACH_RED }); 
+		}
+		else if (MapSystem::get_biome_by_indices(bossIndex) == Biome::B_JUNGLE || MapSystem::get_biome_by_indices(bossIndex) == Biome::B_SAVANNA) {
+			bossSpawns.push_back({ bossIndex, false, CreatureID::BOSS_JUNGLE_YELLOW }); 
+		}
+		else if (MapSystem::get_biome_by_indices(bossIndex) == Biome::B_FOREST) {
+			bossSpawns.push_back({ bossIndex, false, CreatureID::BOSS_FOREST_PURPLE }); 
+		}
+		else {
+			bossSpawns.push_back({ bossIndex, false, CreatureID::BOSS });
+		}
     }
 }
--- src/creature/creature_defs/purple_reaper.hpp
+++ src/creature/creature_defs/purple_reaper.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "creature_definition_data.hpp"
-#include "creature/creature_common.hpp"   // Contains enums like CreatureType, Biome, etc.
-#include "ai/ai_common.hpp"                // For AIConfig defaults.
-#include "ai/state_machine/transition.hpp" // For TransitionTable.
+#include "creature/creature_common.hpp"
+#include "ai/ai_common.hpp"
+#include "ai/state_machine/transition.hpp"
 #include "animation/animation_definition.hpp"
-#include "common.hpp"                      // For global asset definitions.
+#include "common.hpp"
 #include <glm/glm.hpp>
 #include <unordered_map>
 #include <string>
@@ -14,18 +14,10 @@
 #include <creature/creature_defs/ai_defs/ai_purple_reaper_def.hpp>
 #include <animation/animation_manager.hpp>
 
-class PurpleReaperData : public CreatureDefinitionData {
+
+class BaseData : public CreatureDefinitionData {
 public:
-    PurpleReaperData() {
-        creatureID = CreatureID::BOSS;
-        creatureType = CreatureType::Boss;
-        initialize();
-    }
-    virtual ~PurpleReaperData() = default;
-
-    // Explicit initialize method that calls all the initialization functions.
     virtual void initialize() override {
-
         initializeSpawnInfo();
         initializeStats();
         initializeRenderingInfo();
@@ -34,21 +26,9 @@ public:
         initializeAIInfo();
         initializeAnimations();
         initializeUIInfo();
-
-    }
-
-    static const PurpleReaperData& getInstance() {
-        static PurpleReaperData instance;
-        return instance;
     }
 
 protected:
-    virtual void initializeSpawnInfo() override {
-        spawnInfo.spawnProbability = 0.6f;
-        spawnInfo.group = {1, 1};
-        spawnInfo.biomes = {Biome::B_FOREST, Biome::B_BEACH};
-    }
-
     virtual void initializeStats() override {
         stats.minHealth = 500;
         stats.maxHealth = 500;
@@ -56,21 +36,14 @@ protected:
         stats.speed = 1.2f;
     }
 
-    virtual void initializeRenderingInfo() override {
-        renderingInfo.scale = { 80.f, 80.f };
-        renderingInfo.spriteSheet.textureAssetID = TEXTURE_ASSET_ID::MOB;
-        renderingInfo.spriteSheet.sheetDimensions = {240.f, 320.f};
-    }
-
     virtual void initializePhysicsInfo() override {
-        physicsInfo.scale = vec2(100, 100) * 1.5f;
-
-        physicsInfo.offset_to_ground = {0, physicsInfo.scale.y / 2.f};;
+        physicsInfo.scale = vec2(35.f, 56.f) * 2.f;
+        physicsInfo.offset_to_ground = { 0, physicsInfo.scale.y / 2.f };
         float w = physicsInfo.scale.x;
-	    float h = physicsInfo.scale.y;
+        float h = physicsInfo.scale.y;
         physicsInfo.hitbox.pts = {
-            {w * -0.5f * 0.5, h * -0.5f}, {w * 0.5f* 0.5, h * -0.5f},
-            {w * 0.5f * 0.5, h * 0.5f},   {w * -0.5f * 0.5, h * 0.5f}
+            {w * -0.5f, h * -0.5f}, {w * 0.5f, h * -0.5f},
+            {w * 0.5f, h * 0.5f},   {w * -0.5f, h * 0.5f}
         };
         physicsInfo.hitbox.depth = 50;
     }
@@ -106,34 +79,32 @@ protected:
         aiInfo.initialState = "patrol";
     }
 
-
-
     virtual void initializeAnimations() override {
         float frameWidth = renderingInfo.scale.x;
         float frameHeight = renderingInfo.scale.y;
 
         std::string animationHeader = AnimationManager::getInstance().creatureAnimationHeader(creatureID);
 
-       
+
         AnimationDefinition idle_right;
         idle_right.id = AnimationManager::getInstance().buildAnimationKey(animationHeader, MotionAction::IDLE, MotionDirection::RIGHT);
-        idle_right.loop = true; 
+        idle_right.loop = true;
         idle_right.frameWidth = frameWidth;
         idle_right.frameHeight = frameHeight;
         idle_right.spriteSheet = renderingInfo.spriteSheet;
 
-       
+
         idle_right.frames.clear();
         idle_right.frameDurations.clear();
 
-       
-        idle_right.frames.push_back({ 2, 0 }); // Third row, second column
-        idle_right.frames.push_back({ 2, 1 }); // Third row, third column
-        idle_right.frames.push_back({ 2, 2 }); // Third row, fourth column
-        idle_right.frames.push_back({ 3, 0 }); // Fourth row, first column
 
-        // Set duration for each frame (in milliseconds)
-        float frameDuration = 150.0f; 
+        idle_right.frames.push_back({ 0, 6 });
+        idle_right.frames.push_back({ 0, 7 });
+        idle_right.frames.push_back({ 0, 8 });
+        idle_right.frames.push_back({ 0, 9 });
+
+        
+        float frameDuration = 150.0f;
         idle_right.frameDurations.push_back(frameDuration);
         idle_right.frameDurations.push_back(frameDuration);
         idle_right.frameDurations.push_back(frameDuration);
@@ -141,8 +112,8 @@ protected:
 
         AnimationManager::getInstance().registerCreatureAnimation(creatureID, MotionAction::IDLE, MotionDirection::RIGHT, idle_right);
 
-       
-        AnimationDefinition walk_right = idle_right; 
+
+        AnimationDefinition walk_right = idle_right;
         walk_right.id = AnimationManager::getInstance().buildAnimationKey(animationHeader, MotionAction::WALK, MotionDirection::RIGHT);
         AnimationManager::getInstance().registerCreatureAnimation(creatureID, MotionAction::WALK, MotionDirection::RIGHT, walk_right);
 
@@ -150,7 +121,124 @@ protected:
         renderingInfo.initDirection = MotionDirection::RIGHT;
     }
 
+
+   
+
     virtual void initializeUIInfo() override {
-        uiInfo.healthBar_y_adjust = - 10.f;
+        uiInfo.healthBar_y_adjust = -10.f;
     }
 };
+
+// purple
+class ForestPurpleWizardData : public BaseData {
+public:
+    ForestPurpleWizardData() {
+        creatureID = CreatureID::BOSS_FOREST_PURPLE;
+        creatureType = CreatureType::Boss;
+        initialize();
+    }
+
+    static const ForestPurpleWizardData& getInstance() {
+        static ForestPurpleWizardData instance;
+        return instance;
+    }
+
+protected:
+    virtual void initializeSpawnInfo() override {
+        spawnInfo.spawnProbability = 0.6f;
+        spawnInfo.group = { 1, 1 };
+        spawnInfo.biomes = { Biome::B_FOREST };
+    }
+
+    virtual void initializeRenderingInfo() override {
+        renderingInfo.scale = { 35.f, 56.f };
+        renderingInfo.spriteSheet.textureAssetID = TEXTURE_ASSET_ID::MOB_PURPLE;
+        renderingInfo.spriteSheet.sheetDimensions = { 350.f, 56.f };
+    }
+};
+
+// red
+class BeachPurpleWizardData : public BaseData {
+public:
+    BeachPurpleWizardData() {
+        creatureID = CreatureID::BOSS_BEACH_RED;
+        creatureType = CreatureType::Boss;
+        initialize();
+    }
+
+    static const BeachPurpleWizardData& getInstance() {
+        static BeachPurpleWizardData instance;
+        return instance;
+    }
+    
+protected:
+    virtual void initializeSpawnInfo() override {
+        spawnInfo.spawnProbability = 0.6f;
+        spawnInfo.group = { 1, 1 };
+        spawnInfo.biomes = { B_ICE };
+    }
+
+    virtual void initializeRenderingInfo() override {
+        renderingInfo.scale = { 35.f, 56.f };
+        renderingInfo.spriteSheet.textureAssetID = TEXTURE_ASSET_ID::MOB_RED;
+        renderingInfo.spriteSheet.sheetDimensions = { 350.f, 56.f };
+    }
+};
+
+// yellow
+class JunglePurpleWizardData : public BaseData {
+public:
+    JunglePurpleWizardData() {
+        creatureID = CreatureID::BOSS_JUNGLE_YELLOW;
+        creatureType = CreatureType::Boss;
+        initialize();
+    }
+
+    static const JunglePurpleWizardData& getInstance() {
+        static JunglePurpleWizardData instance;
+        return instance;
+    }
+
+protected:
+    virtual void initializeSpawnInfo() override {
+        spawnInfo.spawnProbability = 0.6f;
+        spawnInfo.group = { 1, 1 };
+        spawnInfo.biomes = { Biome::B_JUNGLE, Biome::B_SAVANNA };
+    }
+
+    virtual void initializeRenderingInfo() override {
+        renderingInfo.scale = { 35.f, 56.f };
+        renderingInfo.spriteSheet.textureAssetID = TEXTURE_ASSET_ID::MOB_YELLOW;
+        renderingInfo.spriteSheet.sheetDimensions = { 350.f, 56.f };
+    }
+};
+
+// default blue
+class DefaultWizardData : public BaseData {
+public:
+    DefaultWizardData() {
+        creatureID = CreatureID::BOSS;
+        creatureType = CreatureType::Boss;
+        initialize();
+    }
+
+    static const DefaultWizardData& getInstance() {
+        static DefaultWizardData instance;
+        return instance;
+    }
+
+protected:
+    virtual void initializeSpawnInfo() override {
+        spawnInfo.spawnProbability = 0.6f;
+        spawnInfo.group = { 1, 1 };
+        spawnInfo.biomes = { Biome::B_BEACH}; 
+    }
+
+    virtual void initializeRenderingInfo() override {
+        renderingInfo.scale = { 35.f, 56.f };
+        renderingInfo.spriteSheet.textureAssetID = TEXTURE_ASSET_ID::MOB;
+        renderingInfo.spriteSheet.sheetDimensions = { 350.f, 56.f };
+    }
+};
+
+
